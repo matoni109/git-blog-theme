6e10a1a1c076366d357ed4a1c3130a896fbb6c61
Chris Wade <chris@wadespace.net>
2023-09-11 11:56:09 +1000

    add alphin

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
   <?php wp_head(); ?>
 </head>
 
