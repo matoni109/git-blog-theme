8d059a6d071d9d7cfd51c23e78cbe78cf9741cac
Chris Wade <chris@wadespace.net>
2023-09-20 15:25:03 +1000

    archive remove archive logic from content.php loop

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,42 +1,32 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('mb-12'); ?>>
 
-  <?php if (is_archive()) : ?>
-    <div class="entry-summary">
-      <?php get_template_part('template-parts/search-card')
-      ?>
-    </div>
+  <header class="entry-header mb-4">
+    <?php the_title(sprintf('<h2 class="entry-title text-2xl md:text-3xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+    <time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
+  </header>
 
-  <?php else : ?>
+  <span>its content</span>
+  <div class="entry-content">
+    <?php
+    /* translators: %s: Name of current post */
+    the_content(
+      sprintf(
+        __('Continue reading %s', 'tailpress'),
+        the_title('<span class="screen-reader-text">"', '"</span>', false)
+      )
+    );
 
-    <header class="entry-header mb-4">
-      <?php the_title(sprintf('<h2 class="entry-title text-2xl md:text-3xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-      <time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
-    </header>
-
-    <span>its content</span>
-    <div class="entry-content">
-      <?php
-      /* translators: %s: Name of current post */
-      the_content(
-        sprintf(
-          __('Continue reading %s', 'tailpress'),
-          the_title('<span class="screen-reader-text">"', '"</span>', false)
-        )
-      );
-
-      wp_link_pages(
-        array(
-          'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'tailpress') . '</span>',
-          'after'       => '</div>',
-          'link_before' => '<span>',
-          'link_after'  => '</span>',
-          'pagelink'    => '<span class="screen-reader-text">' . __('Page', 'tailpress') . ' </span>%',
-          'separator'   => '<span class="screen-reader-text">, </span>',
-        )
-      );
-      ?>
-    </div>
-
-  <?php endif; ?>
+    wp_link_pages(
+      array(
+        'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'tailpress') . '</span>',
+        'after'       => '</div>',
+        'link_before' => '<span>',
+        'link_after'  => '</span>',
+        'pagelink'    => '<span class="screen-reader-text">' . __('Page', 'tailpress') . ' </span>%',
+        'separator'   => '<span class="screen-reader-text">, </span>',
+      )
+    );
+    ?>
+  </div>
 
 </article>
