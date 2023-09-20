01b7afe287bbf953d7549b36520a403fc8184a55
Chris Wade <chris@wadespace.net>
2023-09-20 12:32:35 +1000

    index create archive catch all

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php get_header(); ?>
 
 <div class="container mx-auto my-8">
-  <h3>index</h3>
-  <?php if (have_posts()) : ?>
-    <?php
-    while (have_posts()) :
-      the_post();
-    ?>
 
-      <?php get_template_part('template-parts/content', get_post_format()); ?>
-
-    <?php endwhile; ?>
-
-  <?php endif;
-  get_template_part('pagination');
-  ?>
+  <main role="main">
+    <section>
+      <h3>index</h3>
+      <!-- index-items-section -->
+      <?php if (have_posts()) : ?>
+        <span class="search-items grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <?php
+          while (have_posts()) :
+            the_post();
+          ?>
+            <span class="search-item">
+              <?php get_template_part('template-parts/content', get_post_format()); ?>
+            </span>
+          <?php endwhile; ?>
+        </span>
+        <!-- /index-items-section -->
+        <?php get_template_part('pagination'); ?>
+      <?php else : ?>
+        <span class="text-left md:tracking-wide text-inherit">Sorry, but nothing is currently matching your content type.</span>
+      <?php endif; ?>
+    </section>
+  </main>
 </div>
-
-<?php
-get_footer();
+<?php get_footer(); ?>
