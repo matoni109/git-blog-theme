ecc30e157cfa7be2b0a89d082dba098e04fefc2c
Chris Wade <chris@wadespace.net>
2023-09-22 15:56:31 +1000

    post finished tag getter

--- template-parts/components/card-tags-category-getter.php
+++ template-parts/components/card-tags-category-getter.php
@@ -4,29 +4,25 @@
  * Component for category and tags output
  *
  * To be used inside cards
- *
  */
 
 $the_post_id = get_the_ID();
 $article_terms = wp_get_post_terms($the_post_id, ['category', 'post_tag']);
 
-if (empty($article_terms) || !is_array($article_terms)) {
-  return;
-}
-
+if (!empty($article_terms) && is_array($article_terms)) {
 ?>
-<!-- tags -->
-<div class="flex flex-wrap">
-  <?php
-  foreach ($article_terms as $key => $article_term) {
-  ?>
-    <a class="category-item pr-1" href="<?php echo esc_url(get_term_link($article_term)); ?>">
-      <p class="capitalize font-bold text-gradient-purple-coral">
-        <?php echo esc_html($article_term->name); ?>
-      </p>
-    </a>
-  <?php
-  }
-  ?>
-</div>
-<!-- /tags -->
+
+  <!-- tags -->
+  <div class="flex flex-wrap">
+    <?php foreach ($article_terms as $key => $article_term) { ?>
+      <a class="category-item pr-1" href="<?php echo esc_url(get_term_link($article_term)); ?>">
+        <p class="capitalize font-bold text-gradient-purple-coral">
+          <?php echo esc_html($article_term->name); ?>
+        </p>
+      </a>
+    <?php } ?>
+  </div>
+  <!-- /tags -->
+
+<?php } // End of if condition
+?>
--- template-parts/components/post-tags-getter.php
+++ template-parts/components/post-tags-getter.php
@@ -10,38 +10,38 @@
 $tags = get_tags(array(
   'taxonomy' => 'post_tag',
   'orderby' => 'name',
-  'hide_empty' => true // for development
+  'hide_empty' => true
 ));
 
-if (empty($tags) || !is_array($tags)) {
-  return;
-}
+
+if (wp_get_post_terms($post->ID, 'post_tag', ['fields' => 'ids'])) {
+
 ?>
+  <div class="post-tags font-mono my-4 md:mt-6">
+    <div class="flex flex-nowrap items-start">
+      <span class="post-tags-label flex-shrink-0 inline-block mt-2">Tags:</span>
+      <!-- tags -->
+      <ul class="flex flex-wrap inline-block shrink list-none text-primary mt-2 mb-0 ml-4">
+        <?php
+        $tags = get_the_tags(); // Get tags for the current post
 
-<!-- tags -->
-<div class="post-tags font-mono my-4 md:mt-6">
-  <div class="flex flex-nowrap items-start">
-    <span class="post-tags-label flex-shrink-0 inline-block mt-2">Tags:</span>
-    <ul class="flex flex-wrap inline-block shrink list-none text-primary mt-2 mb-0 ml-4">
-      <?php
-      $tags = get_the_tags(); // Get tags for the current post
+        if ($tags) {
+          $tag_names = array();
 
-      if ($tags) {
-        $tag_names = array();
+          foreach ($tags as $tag) {
+            $tag_link = get_tag_link($tag->term_id);
 
-        foreach ($tags as $tag) {
-          $tag_link = get_tag_link($tag->term_id);
+            $tag_names[] = '<li><a href="' . esc_html($tag_link) . '" class="hover:underline capitalize" rel="tag" title="' . esc_html($tag->name) . ' Tag"><p class="break-words ' . esc_html($tag->slug) . '">' . esc_html($tag->name) . '</p></a></li>';
+          }
 
-          $tag_names[] = '<li><a href="' . esc_html($tag_link) . '" class="hover:underline capitalize" rel="tag" title="' . esc_html($tag->name) . ' Tag"><p class="break-words ' . esc_html($tag->slug) . '">' . esc_html($tag->name) . '</p></a></li>';
+          $tags_string = implode('<li><p style="color: black;">,</p></li><li><p style="color: transparent;">_</p></li>', $tag_names);
+
+          echo $tags_string;
         }
-
-        $tags_string = implode('<li><p style="color: black;">,</p></li><li><p style="color: transparent;">_</p></li>', $tag_names);
-
-        echo $tags_string;
-      }
-      ?>
-    </ul>
+        ?>
+      </ul>
+      <!-- /tags -->
+    </div>
   </div>
-</div>
-
-<!-- /tags -->
+<?php }
+?>
