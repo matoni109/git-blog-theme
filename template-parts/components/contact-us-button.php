0d090ed8b39fdf43c8a3b56ba44836e6197807fc
Chris Wade <chris@wadespace.net>
2024-05-03 17:52:13 +1000

    update contact-button ref

--- template-parts/components/contact-us-button.php
+++ template-parts/components/contact-us-button.php
@@ -1,3 +1,3 @@
-<a href="#" class="py-2" aria-label="Contact Us Link">
+<a href="/contact" class="py-2" aria-label="Contact Us Link">
   <button id="contact-us-button" class="rounded-md bg-white px-4 py-3 text-sm tracking-wide font-bold ring-1 ring-inset ring-gray-300 hover:ring-2 hover:ring-inset hover:ring-gray-950 hover:drop-shadow-sm">Contact Us</button>
 </a>
