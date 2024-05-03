239f46ac853e29ae3248572638835250071ac224
Chris Wade <chris@wadespace.net>
2024-05-03 17:55:33 +1000

    update blog-logo comp

--- template-parts/components/blog-logo-name.php
+++ template-parts/components/blog-logo-name.php
@@ -1,15 +1,16 @@
-<div>
+<div class="">
   <?php if (has_custom_logo()) { ?>
     <?php the_custom_logo(); ?>
   <?php } else { ?>
-    <a href="<?php echo get_bloginfo('url'); ?>" class="flex items-center font-extrabold text-lg font-mono">
-      <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/github-mark.svg" class="object-scale-down w-8 mr-4">
-      <?php echo get_bloginfo('name'); ?>
-    </a>
+    <div class="flex flex-col items-start">
+      <a href="<?php echo get_bloginfo('url'); ?>" class="flex items-center font-extrabold text-lg font-mono mb-1">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/github-mark.svg" class="object-scale-down w-8 mr-4">
+        <?php echo get_bloginfo('name'); ?>
+      </a>
 
-    <p class="text-sm font-light text-gray-600">
-      <?php echo get_bloginfo('description'); ?>
-    </p>
-
+      <p class="text-sm font-light text-gray-600">
+        <?php echo get_bloginfo('description'); ?>
+      </p>
+    </div>
   <?php } ?>
 </div>
