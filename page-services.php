12a68bc6289a8cfd622485acc83df979521b9755
Chris Wade <chris@wadespace.net>
2024-11-24 09:00:55 +1000

    services page initial

---
+++ page-services.php
@@ -0,0 +1,261 @@
+<?php
+
+/**
+ * Services Page template file.
+ *
+ */
+
+get_header();
+
+?>
+
+<main>
+
+  <div class="container mx-auto my-8">
+    <main role="main">
+
+      <section>
+
+        <header>
+          <div class="px-4">
+            <div id="results" class="mb-6 pb-3">
+              <h2 class="entry-results text-xxl md:text-4xl lg:text-5xl font-bold leading-tight">
+                Blog
+              </h2>
+            </div>
+          </div>
+        </header>
+
+        <!-- index-items-section -->
+
+        <span class="search-items grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <span class="search-item">
+
+            <article id="post-76" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry category-code category-programming tag-code tag-computing tag-edge">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/getting-started-with-edge-computing/" alt="Getting started with edge computing"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/1200x630-RMP-QA-LI_Banner_v2-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/1200x630-RMP-QA-LI_Banner_v2-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/1200x630-RMP-QA-LI_Banner_v2-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/1200x630-RMP-QA-LI_Banner_v2-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/1200x630-RMP-QA-LI_Banner_v2.webp 1200w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/code/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      code </p>
+                  </a>
+                  <a class="category-item pr-1" href="//localhost:3000/category/programming/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      programming </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/getting-started-with-edge-computing/" rel="bookmark">Getting started with edge computing</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">The Microsoft Azure cloud computing dictionary describes edge computing as a framework that “allows devices in remote locations to process... <a class="view-article" href="//localhost:3000/getting-started-with-edge-computing/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-16T01:04:53+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 16, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+          <span class="search-item">
+
+            <article id="post-73" class="flex flex-col py-6 border-t border-gray-200 post-73 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/codeql-team-uses-ai-to-power-vulnerability-detection-in-code/" alt="CodeQL team uses AI to power vulnerability detection in code"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/1200x630-Security-Unfurl-DARK-Logo-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="CodeQL team uses AI to power vulnerability detection in code" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/1200x630-Security-Unfurl-DARK-Logo-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Security-Unfurl-DARK-Logo-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Security-Unfurl-DARK-Logo-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Security-Unfurl-DARK-Logo.webp 1200w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Uncategorized </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/codeql-team-uses-ai-to-power-vulnerability-detection-in-code/" rel="bookmark">CodeQL team uses AI to power vulnerability detection in code</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">AI is fundamentally changing the technology and security landscape. At GitHub, we see AI as a way for developers to... <a class="view-article" href="//localhost:3000/codeql-team-uses-ai-to-power-vulnerability-detection-in-code/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-16T01:03:04+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 16, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+          <span class="search-item">
+
+            <article id="post-70" class="flex flex-col py-6 border-t border-gray-200 post-70 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/apply-now-for-github-universe-2023-micro-mentoring/" alt="Apply now for GitHub Universe 2023 micro-mentoring"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/1200x630-Blog-Master-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Apply now for GitHub Universe 2023 micro-mentoring" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/1200x630-Blog-Master-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Blog-Master-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Blog-Master-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/1200x630-Blog-Master.webp 1200w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Uncategorized </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/apply-now-for-github-universe-2023-micro-mentoring/" rel="bookmark">Apply now for GitHub Universe 2023 micro-mentoring</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">We’re thrilled to announce, for the fifth year in a row, an opportunity for students to connect with GitHub employees... <a class="view-article" href="//localhost:3000/apply-now-for-github-universe-2023-micro-mentoring/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-16T01:02:15+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 16, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+          <span class="search-item">
+
+            <article id="post-67" class="flex flex-col py-6 border-t border-gray-200 post-67 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/github-availability-report-august-2023/" alt="GitHub Availability Report: August 2023"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/GitHub-availability-report_banner-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="GitHub Availability Report: August 2023" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/GitHub-availability-report_banner-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/GitHub-availability-report_banner-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/GitHub-availability-report_banner-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/GitHub-availability-report_banner.webp 1200w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Uncategorized </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/github-availability-report-august-2023/" rel="bookmark">GitHub Availability Report: August 2023</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">In August, we experienced two incidents that resulted in degraded performance across GitHub services. August 15 16:58 UTC (lasting 4... <a class="view-article" href="//localhost:3000/github-availability-report-august-2023/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-16T01:01:05+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 16, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+          <span class="search-item">
+
+            <article id="post-63" class="flex flex-col py-6 border-t border-gray-200 post-63 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/introducing-auto-triage-rules-for-dependabot/" alt="Introducing auto-triage rules for Dependabot"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Introducing auto-triage rules for Dependabot" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot-1536x806.webp 1536w, //localhost:3000/wp-content/uploads/2023/09/autotriage-dependabot.webp 1600w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Uncategorized </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/introducing-auto-triage-rules-for-dependabot/" rel="bookmark">Introducing auto-triage rules for Dependabot</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">Since the May beta release of our GitHub-curated Dependabot policies that detect and close false positive alerts, over 250k repositories... <a class="view-article" href="//localhost:3000/introducing-auto-triage-rules-for-dependabot/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-16T01:00:05+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 16, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+          <span class="search-item">
+
+            <article id="post-43" class="flex flex-col py-6 border-t border-gray-200 post-43 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <a href="//localhost:3000/why-rust-is-the-most-admired-language-among-developers/" alt="Why Rust is the most admired language among developers"><img width="1024" height="538" src="//localhost:3000/wp-content/uploads/2023/09/rust-1024x538.webp" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Why Rust is the most admired language among developers" loading="lazy" decoding="async" srcset="//localhost:3000/wp-content/uploads/2023/09/rust-1024x538.webp 1024w, //localhost:3000/wp-content/uploads/2023/09/rust-300x158.webp 300w, //localhost:3000/wp-content/uploads/2023/09/rust-768x403.webp 768w, //localhost:3000/wp-content/uploads/2023/09/rust.webp 1200w" sizes="auto, (max-width: 1024px) 100vw, 1024px"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Uncategorized </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="//localhost:3000/why-rust-is-the-most-admired-language-among-developers/" rel="bookmark">Why Rust is the most admired language among developers</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">For the eighth year in a row, Rust has topped the chart as “the most desired programming language” in Stack... <a class="view-article" href="//localhost:3000/why-rust-is-the-most-admired-language-among-developers/"></p>
+                <div class="pt-3">
+
+
+                  <a href="//localhost:3000/author/admin/">
+                    <p class="text-small font-bold capitalize break-words">Christopher Charles</p>
+                  </a>
+
+
+                  <time datetime="2023-09-09T03:41:32+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">September 9, 2023 </time>
+                </div>
+
+              </div>
+
+            </article>
+          </span>
+        </span>
+        <!-- /search-items-section -->
+
+      </section>
+    </main>
+  </div>
+
+</main>
+
+<?php get_footer();
