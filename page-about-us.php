e031f47909546e038d465b1d96c8266e4ff524da
Chris Wade <chris@wadespace.net>
2024-11-23 16:53:29 +1000

    add about us page + update footer

---
+++ page-about-us.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * About Us Page template file.
+ *
+ */
+
+get_header();
+
+?>
+
+<div id="content" class="site-content flex-grow">
+  <main>
+
+    <div class="container mx-auto my-8">
+      <div class="mx-auto">
+        <div class="px-4">
+          <!-- archive header -->
+
+          <header class="page-header mb-4">
+            <span class="flex sm:flex-row flex-col justify-between content-center">
+
+              <div class="author-col-one text-left ml-0">
+                <h1 class="inline-block capitalize text-xxl md:text-4xl lg:text-5xl font-bold">About Us</h1>
+              </div>
+
+            </span>
+
+            <div id="author-description" class="mb-8 mt-3 lg:mt-6">
+              <p class="text-left break-words indent-8">Niyati Tamaskar is a mother, engineer, entrepreneur, public speaker, and author. She speaks on issues of cultural bias, the stigma of cancer, and more. Her speaking and media appearances include her signature TEDx talk, a cover and feature spread in Columbus magazine on her journey and message of destigmatizing cancer, and a video created by Breastcancer.org on “How Niyati Tamaskar Overcame Cultural Cancer Stigma to Become an Advocate”—aimed at highlighting the minority experience while facing cancer.</p>
+            </div>
+
+          </header>
+
+          <main role="main">
+            <section>
+              <!-- archive-items-section -->
+
+              <!-- chris start item-->
+
+              <article id="chris-wade" class="flex flex-col md:flex-row-reverse p-2 py-6 border-t border-gray-200 md:mb-2 post-76 post type-post status-publish format-standard has-post-thumbnail hentry category-code category-programming tag-code tag-computing tag-edge">
+
+                <header class="entry-header basis-1/2 pb-3 md:pb-1 lg:px-3">
+                  <a href="#" alt="Getting started with edge computing"><img width="582" height="582" src="https://res.cloudinary.com/oeelsafe/image/upload/v1627111944/r88j7skh1f3mpb4pd8lah4nag3kc.webp" class="rounded object-cover object-scale-up tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing"></a>
+                </header>
+
+                <div class="entry-summary md:px-3 basis-1/2">
+
+                  <!-- Tags -->
+                  <div class="flex flex-wrap">
+                    <a class="category-item pr-1" href="//git-hub-blog.local:3000/tag/code/">
+                      <p class="capitalize font-bold text-gradient-purple-coral">
+                        web development </p>
+                    </a>
+
+                  </div>
+                  <!-- /Tags -->
+
+                  <h2 class="entry-title text-xl md:text-3xl font-extrabold leading-tight mb-1 pt-1"><a href="/getting-started-with-edge-computing/" rel="bookmark">Getting started with edge computing</a></h2>
+                  <p class="flex flex-col text-gray-600 break-words pt-2">The Microsoft Azure cloud computing dictionary describes edge computing as a framework that “allows devices in remote locations to process data at the ‘edge’ of the network, either by the device or a local server. And when data needs to...</p>
+                  <div class="flex items-center pt-2">
+
+                    <span class="flex flex-col pt-2">
+
+                      <time datetime="2023-09-16T01:04:53+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">
+                        Web Developer / Project Management</time>
+                    </span>
+                  </div>
+
+                </div>
+
+              </article>
+
+              <!-- chris end item-->
+
+
+              <!-- emily start item-->
+
+
+              <article id="emily-brewin" class="flex flex-col md:flex-row-reverse p-2 py-6 border-t border-gray-200 md:mb-2 post-76 post type-post status-publish format-standard has-post-thumbnail hentry category-code category-programming tag-code tag-computing tag-edge">
+
+                <header class="entry-header basis-1/2 pb-3 md:pb-1 lg:px-3">
+                  <a href="//git-hub-blog.local:3000/getting-started-with-edge-computing/" alt="Getting started with edge computing"><img width="582" height="582" src="https://emilybrewin.com/wp-content/uploads/2024/02/EBrewin_headshot.webp" class="rounded object-cover object-scale-down tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing"></a>
+                </header>
+
+                <div class="entry-summary md:px-3 basis-1/2">
+
+                  <!-- Tags -->
+                  <div class="flex flex-wrap">
+                    <a class="category-item pr-1" href="//git-hub-blog.local:3000/tag/code/">
+                      <p class="capitalize font-bold text-gradient-purple-coral">
+                        Copy Writer </p>
+                    </a>
+
+                  </div>
+                  <!-- /Tags -->
+
+                  <h2 class="entry-title text-xl md:text-3xl font-extrabold leading-tight mb-1 pt-1"><a href="https://emilybrewin.com/" rel="bookmark" target="_blank">Emily Brewin</a></h2>
+                  <p class="flex flex-col text-gray-600 break-words pt-2">Emily Brewin is a Melbourne author, copywriter, and learning designer. Her first novel, Hello, Goodbye, was published in 2017 and her second, Small Blessings, in 2019.</p>
+                  <p class="flex-col text-gray-600 break-words pt-2">A Way Home is her first YA novel. Her short stories have appeared in anthologies, Into Your Arms: Nick Cave’s Songs Reimagined by Fremantle Press, We’ll Stand In That Place and Other Stories by Margaret River Press and the Bristol Short Story Anthology - Volume 10.</p>
+                  <p class="flex-col text-gray-600 break-words pt-2">She has written for The Age, ABC Education, news.com.au, Meanjin, Kill Your Darlings, Archer, Feminartsy, The Victorian Writer, Screen Education, and Mamamia. She is currently working on her fourth novel.</p>
+                  <div class="flex items-center pt-2">
+
+                    <span class="flex flex-col pt-2">
+                      <time datetime="2023-09-16T01:04:53+00:00" itemprop="datePublished" class="font-mono text-sm text-gray-700">
+                        Copy Writer / SEO Specalist</time>
+                    </span>
+                  </div>
+
+                </div>
+
+              </article>
+
+              <!-- emily end item-->
+
+              <!-- /archive-items-section -->
+            </section>
+          </main>
+        </div>
+      </div>
+    </div>
+  </main>
+
+</div>
+
+<?php get_footer();
--- template-parts/footer/footer-top.php
+++ template-parts/footer/footer-top.php
@@ -5,7 +5,7 @@
       <a href="<?php echo get_bloginfo('url'); ?>" class="flex-none" aria-label="Go to enceladus digital homepage">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/images/enceladus-moon.svg" class="object-scale-up h-14" aria-label="logo-enceladus">
       </a>
-      <p class="flex-auto font-bold text-gray-800 md:text-lg text-xxl font-mono text-center"><?php echo get_bloginfo('name'); ?>
+      <p class="flex-auto font-bold text-gray-800 md:text-lg text-xl font-mono text-center"><?php echo get_bloginfo('name'); ?>
       <p>
     </span>
   </div>
@@ -20,29 +20,29 @@
     </ul>
   </div>
 
-  <div class="basis-1/2 md:flex-auto mb-6 md:mb-2 pr-3 pr-md-0 md:pl-4">
+  <!-- <div class="basis-1/2 md:flex-auto mb-6 md:mb-2 pr-3 pr-md-0 md:pl-4">
     <h5 class="h5 mb-3 font-mono">About Us</h5>
     <ul class="list-none">
       <li class="mb-3"><a href="./about-us" class="link-gray">About Us</a></li>
     </ul>
-  </div>
+  </div> -->
 
   <div class="basis-1/2 md:flex-auto mb-6 md:mb-2 pr-3 md:pr-0 md:pl-4">
     <h5 class="h5 mb-3 font-mono">Site</h5>
     <ul class="list-none">
       <li class="mb-3"><a href="./about-us" class="link-gray">About</a></li>
-      <li class="mb-3"><a href="./services" class="link-gray">Services</a></li>
+      <!-- <li class="mb-3"><a href="./services" class="link-gray">Services</a></li> -->
       <li class="mb-3"><a href="./projects" class="link-gray">Projects</a></li>
       <li class="mb-3"><a href="./blog" class="link-gray">Blog</a></li>
       <li class="mb-3"><a href="./contact" class="link-gray">Contact</a></li>
     </ul>
   </div>
 
-  <div class="basis-1/2 md:flex-auto mb-6 md:mb-2 pr-3 md:pr-0 md:pl-4">
+  <!-- <div class="basis-1/2 md:flex-auto mb-6 md:mb-2 pr-3 md:pr-0 md:pl-4">
     <h5 class="h5 mb-3 font-mono">Projects</h5>
     <ul class="list-none">
       <li class="mb-3"><a href="./projects" class="link-gray">Projects</a></li>
     </ul>
-  </div>
+  </div> -->
 
 </div>
