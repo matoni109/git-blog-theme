3ec80a71526b495cd9c182a15021a8f63b0332b7
Chris Wade <chris@wadespace.net>
2024-11-24 13:38:54 +1000

    update projects page:

--- page-projects.php
+++ page-projects.php
@@ -31,7 +31,6 @@ get_header();
 
                 <div id="author-description" class="mb-8 mt-3 lg:mt-6">
                   <p class="text-left break-words indent-8 text-gray-600 break-words">At Enceladus Digital, we don’t just deliver results—we create stories of success. From crafting high-performing websites to driving impactful digital campaigns, every project we take on is a reflection of our passion for excellence and our commitment to helping businesses grow.</p>
-                  <!-- <p class="pt-2 text-gray-600 break-words">Explore our portfolio to see how we’ve helped businesses just like yours. Each project tells a unique story of collaboration, innovation, and growth.</p> -->
 
                   <p class="pt-2 text-gray-600 break-words">Ready to see your brand featured here? Let’s create something incredible together. Reach out today and let’s turn your next big idea into a standout success!</p>
                 </div>
@@ -47,26 +46,23 @@ get_header();
                   <article id="post-76" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry">
 
                     <header class="entry-header">
-                      <a class="" href="https://www.nillmc.com.au/lifelong-medical-centres/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/nillumbik-medical-center-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async"></a>
+                      <a class="" href="https://www.nillmc.com.au/lifelong-medical-centres/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/nillumbik-medical-center-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Image of Nillumbik Medical Center Website" loading="lazy" decoding="async"></a>
                     </header>
 
                     <div class="entry-summary">
 
                       <!-- Tags -->
                       <div class="flex flex-wrap">
-                        <!-- <a class="category-item pr-1" href="./category/projects/"> -->
                         <p class="capitalize font-bold text-gradient-purple-coral">
                           medical </p>
-                        <!-- </a> -->
                       </div>
                       <!-- /Tags -->
 
                       <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="https://www.nillmc.com.au/lifelong-medical-centres/" rel="bookmark" target="_blank">Lifelong Health</a></h2>
-                      <p class="flex flex-col text-gray-600 break-words">This project was managed end to end, from inital concept and WordPress theme design, all the way with services including copy writing, and google analytics setup & reporting.</p>
-                      <p class="flex flex-col text-gray-600 break-words pt-2">All five clinic sites with developed and deployed on our hosting services via digital ocean.</p>
+                      <p class="flex flex-col text-gray-600 break-words">This project was managed end to end, from initial concept and WordPress theme design, all the way with services including copywriting and Google Analytics setup & reporting.</p>
+                      <p class="flex flex-col text-gray-600 break-words pt-2">All five clinic sites were developed and deployed on our hosting services via DigitalOcean.</p>
 
                       <div class="flex items-center pt-2">
-
                         <span class="flex flex-col pt-2">
                           <span class="font-mono text-sm text-gray-700">
                             Web Development / Copywriting / SEO</span>
@@ -74,76 +70,63 @@ get_header();
                       </div>
 
                     </div>
-
-
                   </article>
                 </span>
-
-
 
                 <span id="sherbourne" class="search-item">
 
                   <article id="post-76" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry">
 
                     <header class="entry-header">
-                      <a class="" href="https://sherbourneroadmc.net.au/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/sherbourneroad-medical-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async"></a>
+                      <a class="" href="https://sherbourneroadmc.net.au/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/sherbourneroad-medical-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Image of Sherbourne Road Medical Center Website" loading="lazy" decoding="async"></a>
                     </header>
 
                     <div class="entry-summary">
 
                       <!-- Tags -->
                       <div class="flex flex-wrap">
-                        <!-- <a class="category-item pr-1" href="./category/projects/"> -->
                         <p class="capitalize font-bold text-gradient-purple-coral">
                           medical </p>
-                        <!-- </a> -->
                       </div>
                       <!-- /Tags -->
 
                       <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="https://sherbourneroadmc.net.au/" rel="bookmark" target="_blank">Sherbourne Medical</a></h2>
-                      <p class="flex flex-col text-gray-600 break-words">This project was managed end to end, from inital concept and WordPress theme design, all the way with services including copy writing, and google analytics setup & reporting.</p>
-                      <p class="flex flex-col text-gray-600 break-words pt-2">All five clinic sites with developed and deployed on our hosting services via digital ocean.</p>
+                      <p class="flex flex-col text-gray-600 break-words">Sherbourne approached us on the back of the work done for Lifelong Health Care.</p>
+                      <p class="flex flex-col text-gray-600 break-words pt-2">We found a suitable medical-based theme and migrated all of their existing content over to the new instance. The result is a responsive design that can be built upon for direct newsletter marketing and a responsive user experience.</p>
 
                       <div class="flex items-center pt-2">
-
                         <span class="flex flex-col pt-2">
                           <span class="font-mono text-sm text-gray-700">
-                            Web Development / Copywriting / SEO</span>
+                            Web Development / WordPress / Pagespeed</span>
                         </span>
                       </div>
 
                     </div>
-
-
                   </article>
                 </span>
-
 
                 <span id="cycle-collective" class="search-item">
 
                   <article id="post-77" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry">
 
                     <header class="entry-header">
-                      <a href="https://cyclecollective.com.au/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/cycle-collective-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async"></a>
+                      <a href="https://cyclecollective.com.au/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/cycle-collective-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Image of Cycle Collective Website" loading="lazy" decoding="async"></a>
                     </header>
 
                     <div class="entry-summary">
 
                       <!-- Tags -->
                       <div class="flex flex-wrap">
-                        <!-- <a class="category-item pr-1" href="./category/projects/"> -->
                         <p class="capitalize font-bold text-gradient-purple-coral">
                           fitness </p>
-                        <!-- </a> -->
                       </div>
                       <!-- /Tags -->
 
                       <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="https://cyclecollective.com.au/" rel="bookmark" target="_blank">Cycle Collective</a></h2>
-                      <p class="flex flex-col text-gray-600 break-words">We took over an existing WordPress project and updated design elements, user flow, and Google Tag tracking. The project also involve a MindBody intergration and maintenance</p>
-                      <p class="flex flex-col text-gray-600 break-words pt-2">Website continues to see high user engagement and low bounce rate.</p>
+                      <p class="flex flex-col text-gray-600 break-words">We took over an existing WordPress project and updated design elements, user flow, and Google Tag tracking. The project also involved a MindBody integration and maintenance.</p>
+                      <p class="flex flex-col text-gray-600 break-words pt-2">The website continues to see high user engagement and a low bounce rate.</p>
 
                       <div class="flex items-center pt-2">
-
                         <span class="flex flex-col pt-2">
                           <span class="font-mono text-sm text-gray-700">
                             Web Development / Pagespeed / Google</span>
@@ -151,15 +134,40 @@ get_header();
                       </div>
 
                     </div>
-
-
                   </article>
                 </span>
 
+                <span id="collective-on-demand" class="search-item">
 
+                  <article id="post-77" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry">
 
+                    <header class="entry-header">
+                      <a href="https://ondemand.cyclecollective.com.au/" rel="bookmark" target="_blank"><img src="/wp-content/uploads/2024/11/collective-on-demand-website.jpg" class="rounded h-auto object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async"></a>
+                    </header>
 
+                    <div class="entry-summary">
 
+                      <!-- Tags -->
+                      <div class="flex flex-wrap">
+                        <p class="capitalize font-bold text-gradient-purple-coral">
+                          fitness education </p>
+                      </div>
+                      <!-- /Tags -->
+
+                      <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="https://ondemand.cyclecollective.com.au/" rel="bookmark" target="_blank">Cycle Collective</a></h2>
+                      <p class="flex flex-col text-gray-600 break-words">The project was a complete audio fitness & spin instructor learning platform design and implementation. The complete solution included WordPress with LearnDash LMS, Memberpress for payments and subscriptions. </p>
+                      <p class="flex flex-col text-gray-600 break-words pt-2">Contact Enceladus to talk through you next platform idea.</p>
+
+                      <div class="flex items-center pt-2">
+                        <span class="flex flex-col pt-2">
+                          <span class="font-mono text-sm text-gray-700">
+                            App Development / Subscriptions / Learning Management System</span>
+                        </span>
+                      </div>
+
+                    </div>
+                  </article>
+                </span>
 
               </span>
               <!-- /search-items-section -->
