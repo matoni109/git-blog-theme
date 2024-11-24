e3dbe6bf94cd0a681c76e223f18616f02632a709
Chris Wade <chris@wadespace.net>
2024-11-24 12:34:04 +1000

    Add and update projects page

---
+++ page-projects.php
@@ -0,0 +1,223 @@
+<?php
+
+/**
+ * Projects Page template file.
+ *
+ */
+
+get_header();
+
+?>
+
+<main>
+
+  <div class="container mx-auto my-8">
+    <main role="main">
+
+      <section>
+
+        <header class="page-header mb-4">
+          <span class="flex sm:flex-row flex-col justify-between content-center">
+
+            <div class="author-col-one text-left ml-0">
+              <h1 class="inline-block capitalize text-xxl md:text-4xl lg:text-5xl font-bold">Projects</h1>
+            </div>
+
+          </span>
+
+          <div id="author-description" class="mb-8 mt-3 lg:mt-6">
+            <p class="text-left break-words indent-8 text-gray-600 break-words">At Enceladus Digital, we don’t just deliver results—we create stories of success. From crafting high-performing websites to driving impactful digital campaigns, every project we take on is a reflection of our passion for excellence and our commitment to helping businesses grow.</p>
+            <p class="pt-2 text-gray-600 break-words">Explore our portfolio to see how we’ve helped businesses just like yours. Each project tells a unique story of collaboration, innovation, and growth.</p>
+
+            <p class="pt-2 text-gray-600 break-words">Ready to see your brand featured here? Let’s create something incredible together. Reach out today and let’s turn your next big idea into a standout success!</p>
+          </div>
+
+        </header>
+
+        <!-- index-items-section -->
+
+        <span class="search-items grid gap-4 grid-cols-1 md:grid-cols-2">
+
+          <span id="lifelong" class="search-item">
+
+            <article id="post-76" class="flex flex-col py-6 border-t border-gray-200 post-76 post type-post status-publish format-standard has-post-thumbnail hentry category-code category-programming tag-code tag-computing tag-edge">
+
+              <header class="entry-header">
+                <a href="https://www.nillmc.com.au/lifelong-medical-centres/" rel="bookmark" target="_blank"><img width="1024" height="538" src="/wp-content/uploads/2024/11/seo-basics-to-kickstart-your-business.jpg" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Getting started with edge computing" loading="lazy" decoding="async"></a>
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <!-- <a class="category-item pr-1" href="./category/projects/"> -->
+                  <p class="capitalize font-bold text-gradient-purple-coral">
+                    medical </p>
+                  <!-- </a> -->
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1"><a href="https://www.nillmc.com.au/lifelong-medical-centres/" rel="bookmark" target="_blank">Lifelong Health</a></h2>
+                <p class="flex flex-col text-gray-600 break-words">This project was managed end to end, from inital concept and WordPress theme design, all the way with services including copy writing, and google analytics setup & reporting.</p>
+                <p class="flex flex-col text-gray-600 break-words pt-2">All five clinic sites with developed and deployed on our hosting services via digital ocean.</p>
+
+                <div class="flex items-center pt-2">
+
+                  <span class="flex flex-col pt-2">
+                    <span class="font-mono text-sm text-gray-700">
+                      Web Development / Copywriting / SEO</span>
+                  </span>
+                </div>
+
+              </div>
+
+
+            </article>
+          </span>
+
+          <span id="copywriting" class="search-item">
+
+            <article id="post-73" class="flex flex-col py-6 border-t border-gray-200 post-73 post type-post status-publish format-standard has-post-thumbnail hentry category-copywriting">
+
+              <header class="entry-header">
+                <img width="1024" height="538" src="/wp-content/uploads/2024/11/copy-writing-services.jpg" class=" rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="" loading="lazy" decoding="async">
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="./category/copywriting/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Copywriting </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1">Copywriting</h2>
+                <p class="flex flex-col text-gray-600 break-words">Bring your brand to life with powerful copywriting. From websites to ads, our words don’t just tell your story—they inspire action and build connections.</p>
+
+              </div>
+
+            </article>
+          </span>
+
+
+          <span id="web-design" class="search-item">
+
+            <article id="post-70" class="flex flex-col py-6 border-t border-gray-200 post-70 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <img width="1024" height="538" src="/wp-content/uploads/2024/11/ai-and-seo-1024x683-1.jpg" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="" loading="lazy" decoding="async">
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="./category/web-design">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Web Design </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1">Web Design</h2>
+                <p class="flex flex-col text-gray-600 break-words">Create a lasting impression with stunning, user-friendly web design. We build websites that look great, perform seamlessly, and deliver an exceptional user experience.</p>
+
+              </div>
+
+            </article>
+          </span>
+
+          <span id="google-ads" class="search-item">
+
+            <article id="post-67" class="flex flex-col py-6 border-t border-gray-200 post-67 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <img width="1024" height="538" src="/wp-content/uploads/2024/11/key-reasons-for-website-bounce-rate-1024x683-1.jpg" class="rounded object-cover object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="GitHub Availability Report: August 2023" loading="lazy" decoding="async">
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="./category/google-ads/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Google Ads </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1">Google Advertising</h2>
+                <p class="flex flex-col text-gray-600 break-words">Reach your audience when it matters most. Our Google Ads campaigns are designed to drive traffic, increase conversions, and maximise your return on investment.</p>
+
+              </div>
+
+            </article>
+          </span>
+
+          <span id="conversion" class="search-item">
+
+            <article id="post-63" class="flex flex-col py-6 border-t border-gray-200 post-63 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <img src="/wp-content/uploads/2024/11/creating-interactive-web-pages.jpg" class="rounded object-cover pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Introducing auto-triage rules for Dependabot" loading="lazy" decoding="async">
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Conversion Optimisation </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1">Conversion Optimisation</h2>
+                <p class="flex flex-col text-gray-600 break-words">Turn visitors into customers with smart conversion optimisation strategies that refine your website’s user experience and maximise the impact of every click</p>
+
+              </div>
+
+            </article>
+          </span>
+
+          <span id="social-media" class="search-item">
+
+            <article id="post-43" class="flex flex-col py-6 border-t border-gray-200 post-43 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+
+              <header class="entry-header">
+                <img width="1024" height="538" src="/wp-content/uploads/2024/11/evolution-of-seo-1024x892-1.jpg" class="rounded object-scale-down pb-2 tease-thumbnail tease-thumbnail__img wp-post-image" alt="Why Rust is the most admired language among developers" loading="lazy" decoding="async">
+              </header>
+
+              <div class="entry-summary">
+
+                <!-- Tags -->
+                <div class="flex flex-wrap">
+                  <a class="category-item pr-1" href="//localhost:3000/category/uncategorized/">
+                    <p class="capitalize font-bold text-gradient-purple-coral">
+                      Social Media </p>
+                  </a>
+                </div>
+                <!-- /Tags -->
+
+                <h2 class="entry-title break-words text-xl md:text-2xl lg:text-3xl font-extrabold leading-tight mb-3 pt-1">Social Media Marketing</h2>
+                <p class="flex flex-col text-gray-600 break-words">Engage your audience with compelling social media campaigns that build brand awareness, foster connections, and drive results across platforms like Facebook, Instagram, and LinkedIn.</p>
+
+              </div>
+
+            </article>
+          </span>
+
+        </span>
+        <!-- /search-items-section -->
+
+      </section>
+    </main>
+  </div>
+
+</main>
+
+<?php get_footer();
